e2470d9e9078f86e168e992f6e068dedc97c0115
PR0C355 <tumi.oguntola@gmail.com>
2022-01-28 15:13:45 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/SparkTest.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class SparkTest extends SubsystemBase {
+  public CANSparkMax sparkMax_1 = new CANSparkMax(13, MotorType.kBrushless);
+  public CANSparkMax sparkMax_2 = new CANSparkMax(14, MotorType.kBrushless);
+  
+  
+
+  
+  /** Creates a new ExampleSubsystem. */
+  public SparkTest() {
+
+  }
+
+  public void runSparks (double power) {
+    sparkMax_1.set(power);
+    sparkMax_2.set(power);
+  }
+
+  public void runSpark1 (double power) {
+    sparkMax_1.set(power);
+  }
+
+  public void runSpark2 (double power) {
+    sparkMax_2.set(power);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
